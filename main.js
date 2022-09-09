ce2c2b49f4ada34fd64756739093f32c1e293a4f
terremoth <dutr4@outlook.com>
2022-09-09 18:34:43 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const stack = [];
+
+function engine() {
+
+}
+
+function processSingleInstruction(asmOpCode, arg1 = null, arg2 = null) {
+    
+}
+
+function processInstructions() {
+
+}
+
+function asmToHexaMachineCodeTranslator(asmInstruction) {
+    
+}
